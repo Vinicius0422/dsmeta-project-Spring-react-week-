916aa948491602a062bc7f4487d3a775fe610103
Vinicius0422 <guedescosta123@hotmail.com>
2022-09-11 00:39:57 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
-    return (
+  return (
+    <>
       <h1>Hello world!</h1>
-    )
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/images/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar"></img>
+        </div>
+    )
+}
+
+export default NotificationButton
